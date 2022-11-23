1c6bfd0b1ae1b9c0ea4451784819c9290198b423
igorchagov <igorchagov@mail.ru>
2022-11-23 15:04:13 +0300

    add

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+let tl = gsap.timeline();
+
+tl.from('.header-mail',{y: -200, opacity: 0,});
+tl.from('.header-phone', { y: -200, opacity: 0, }, "-=0.5");
+tl.from('.title-logo', { width: 0, opacity: 0, });
+tl.from('.title-text', { x: 110, opacity: 0, });
+tl.from('.img-1', { y: 100, opacity: 0,});
+tl.from('.img-2', { x: 100, y: -200, opacity: 0,});
+tl.from('.img-3', { x: -100, opacity: 0,});
+tl.from('.img-4', { y: 200, opacity: 0,});
+tl.from('.about-item', { x: -200, opacity: 0, } ,'-=2.5');
+tl.from('.soon', { x: -200, opacity: 0, }, '-=1');
+tl.from('.anim',{x: 200, opacity: 0});
